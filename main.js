5d7db4e9330586536e77f1ecf58f87054c420c8f
Trần Ngọc Ly <tranngocly2k6@gmail.com>
2025-04-26 10:08:12 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-function isPrem ()
+let currentProduct = []
+function thembanhmi(){
+    currentProduct.push("Bánh Mì");
+    hienthisanpham();
+
+}
+function themsuachua(){
+    currentProduct.push("Sữa Chua");
+    hienthisanpham();
+}
+function xoa(i){
+    const newProduct = [ ]
+    for(let j = 0; j < currentProduct.length; j++){
+        if(j!== i){
+            newProduct.push(currentProduct[j]);
+        }
+    }
+    currentProduct = newProduct;
+    hienthisanpham();
+
+}
+
+function hienthisanpham(){
+    productList.innerHTML = "";
+    for(let i = 0; i < currentProduct.length; i++){
+        const product  = currentProduct[i];
+        productList.innerHTML += `<li>${product}</li>
+        <button onclick="xoa(${i})">Xóa</button>`
+
+}
+}
+hienthisanpham()
